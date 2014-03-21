1ccf6dd911a2ff82bf7e61a15416b746c2ea42d9
Yossi Gruner <ygruner@interwise.com>
2014-03-21 18:36:42 +0200

    add Android-Universal-Image-Loader for user images

--- android/src/com/att/team/keeper/TeamKeeperApplication.java
+++ android/src/com/att/team/keeper/TeamKeeperApplication.java
@@ -1,16 +1,39 @@
 package com.att.team.keeper;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.att.team.keeper.services.BluetoothService;
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 
 public class TeamKeeperApplication extends Application {
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		
+
 		BluetoothService.INSTANCE.init(this.getApplicationContext());
+		initImageLoader(getApplicationContext());
 	}
-	
+
+	public static void initImageLoader(Context context) {
+		// This configuration tuning is custom. You can tune every option, you
+		// may tune some of them,
+		// or you can create default configuration by
+		// ImageLoaderConfiguration.createDefault(this);
+		// method.
+		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+				context).threadPriority(Thread.NORM_PRIORITY - 2)
+				.denyCacheImageMultipleSizesInMemory()
+				.discCacheFileNameGenerator(new Md5FileNameGenerator())
+				.tasksProcessingOrder(QueueProcessingType.LIFO)
+				.writeDebugLogs() // Remove for release app
+				.build();
+		// Initialize ImageLoader with configuration.
+		ImageLoader.getInstance().init(config);
+	}
+
 }
--- android/src/com/att/team/keeper/fragments/WatchListUsersFragment.java
+++ android/src/com/att/team/keeper/fragments/WatchListUsersFragment.java
@@ -2,13 +2,14 @@ package com.att.team.keeper.fragments;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 import android.app.Fragment;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -28,10 +29,21 @@ import com.att.team.keeper.activities.MobileLostPanicActivity;
 import com.att.team.keeper.dtos.MemberDto;
 import com.att.team.keeper.services.BluetoothService;
 import com.att.team.keeper.services.BluetoothService.IResponseListener;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
 
 public class WatchListUsersFragment extends Fragment implements
 		IResponseListener {
 
+	protected ImageLoader imageLoader = ImageLoader.getInstance();
+	AnimateFirstDisplayListener animateFirstListener = new AnimateFirstDisplayListener();
+	DisplayImageOptions options;
+
+
+	
 	private UsersAdapter mAdapter;
 
 	private ListView mUsersList;
@@ -44,6 +56,18 @@ public class WatchListUsersFragment extends Fragment implements
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+
+		options = new DisplayImageOptions.Builder()
+		.showImageOnLoading(R.drawable.ic_launcher)
+		.showImageForEmptyUri(R.drawable.ic_launcher)
+		.showImageOnFail(R.drawable.ic_launcher)
+		.cacheInMemory(true)
+		.cacheOnDisc(true)
+		.considerExifParams(true)
+		.displayer(new RoundedBitmapDisplayer(20))
+		.build();
+		
+		
 		mInflater = inflater;
 		View view = inflater.inflate(R.layout.watch_users_layout, null);
 
@@ -153,7 +177,8 @@ public class WatchListUsersFragment extends Fragment implements
 			String imageUrl = member.getImageUrl();
 			if (imageUrl != null) {
 				// show The Image
-				new DownloadImageTask(image).execute(imageUrl);
+				imageLoader.displayImage(imageUrl, image, options, animateFirstListener);
+
 			}
 			
 //			if (member.getPanic() == "Panic") {				
@@ -171,6 +196,24 @@ public class WatchListUsersFragment extends Fragment implements
 
 	}
 
+	
+	private static class AnimateFirstDisplayListener extends SimpleImageLoadingListener {
+
+		static final List<String> displayedImages = Collections.synchronizedList(new LinkedList<String>());
+
+		@Override
+		public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+			if (loadedImage != null) {
+				ImageView imageView = (ImageView) view;
+				boolean firstDisplay = !displayedImages.contains(imageUri);
+				if (firstDisplay) {
+					FadeInBitmapDisplayer.animate(imageView, 500);
+					displayedImages.add(imageUri);
+				}
+			}
+		}
+	}
+	
 	private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
 		ImageView bmImage;
 
